3e6f61388faa9d8d313a5fc447e7cee5e863e9c2
Yogesh Pardeshi <105046206+YogeshPardeshi12@users.noreply.github.com>
2023-11-25 15:40:35 +0530

    Add files via upload

---
+++ FruitController.cs
@@ -0,0 +1,122 @@
+﻿using Lemon_Task.Models;
+using System;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lemon_Task.Controllers
+{
+    public class FruitController : Controller
+    {
+        public Yogesh_Entity db = new Yogesh_Entity();
+        // GET: Fruit
+        public ActionResult Index()
+        {
+             var fruits_tbl = db.Fruits_Table.Include(f=>f.Veg_id);
+            return View(fruits_tbl.ToList());
+        }
+        [HttpGet]
+        public ActionResult Details()
+        {
+            var items = db.Fruits_Table.ToList();
+            if (items != null)
+            {
+                ViewBag.data = items;
+            }
+
+            return View("Index");
+        }
+        public ActionResult Create()
+        {
+            ViewBag.veg_Id = new SelectList(db.Vegetables_Table, "veg_Id", "veg_Name");
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Fruit_id,Fruit_name,veg_Id")] Fruits_Table fruits_tbl)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Fruits_Table.Add(fruits_tbl);
+                db.SaveChanges();
+                TempData["message"] = "Your Data Save Successfuly..";
+
+
+                return RedirectToAction("Index");
+
+            }
+
+            ViewBag.veg_Id = new SelectList(db.Vegetables_Table, "veg_Id", "veg_Name", fruits_tbl.Veg_id);
+            return View(fruits_tbl);
+        }
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Fruits_Table fruits_tbl = db.Fruits_Table.Find(id);
+            if (fruits_tbl == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.veg_Id = new SelectList(db.Vegetables_Table, "veg_Id", "veg_Name", fruits_tbl.Veg_id);
+            return View(fruits_tbl);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Fruit_id,Fruit_name,veg_Id")] Fruits_Table fruits_tbl)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(fruits_tbl).State = EntityState.Modified;
+                db.SaveChanges();
+                TempData["message"] = "Your Data Update Successfuly";
+
+                return RedirectToAction("Index");
+            }
+            ViewBag.veg_Id = new SelectList(db.Vegetables_Table, "veg_Id", "veg_Name", fruits_tbl.Veg_id);
+            return View(fruits_tbl);
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Fruits_Table fruits_tbl = db.Fruits_Table.Find(id);
+            if (fruits_tbl == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fruits_tbl);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Fruits_Table fruits_tbl = db.Fruits_Table.Find(id);
+            db.Fruits_Table.Remove(fruits_tbl);
+            db.Entry(fruits_tbl).State = EntityState.Deleted;
+            TempData["Delete"] = "Data Delete Sucessfully!";
+
+
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TreeController.cs
@@ -0,0 +1,127 @@
+﻿using Lemon_Maschine_Test.Data_Access_layer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lemon_Maschine_Test.Controllers
+{
+    public class TreeController : Controller
+    {
+        
+        public DB_AnkitEntities db = new DB_AnkitEntities();
+        // GET: Tree
+        public ActionResult TreeView()
+        {
+            ViewBag.NodeTable = NodeDropdownlist();
+
+            return View();
+        }
+        public ActionResult NodePartialView()
+        {
+            ViewBag.NodeTable = NodeDropdownlist();
+            return View();
+        }
+       
+
+        public ActionResult ReportNodes()
+        {
+            using(DB_AnkitEntities db = new DB_AnkitEntities())
+            return View(db);
+        }
+        public ActionResult SaveOrUpdate(NodeTable node)
+        {
+            try
+            {
+                using (DB_AnkitEntities db = new DB_AnkitEntities())
+                {
+                    if (node.NodeId == 0)
+                    {
+                        NodeTable table = new NodeTable()
+                        {
+                            NodeId = node.NodeId,
+                            NodeName = node.NodeName,
+                            ParentNodeId = node.ParentNodeId,
+                            IsActive = node.IsActive,
+                            StartDate = node.StartDate
+                        };
+                        db.Entry(table).State = System.Data.Entity.EntityState.Added;
+                    }
+                    else
+                    {
+                        NodeTable table = new NodeTable()
+                        {
+                            NodeId = node.NodeId,
+                            NodeName = node.NodeName,
+                            ParentNodeId = node.ParentNodeId,
+                            IsActive = node.IsActive,
+                            StartDate = node.StartDate
+                        };
+                        db.Entry(table).State = System.Data.Entity.EntityState.Modified;
+                    }
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return RedirectToAction("NodePartialView");
+            }
+
+        public ActionResult Detail(int id)
+        {
+            using (DB_AnkitEntities db = new DB_AnkitEntities())
+            {
+                var getdetail = db.NodeTables.Find(id);
+                return View();
+            }
+        }
+        public ActionResult EditData(int id)
+        {
+
+            using (DB_AnkitEntities db = new DB_AnkitEntities())
+            {
+                var data = new NodeTable();
+                data = db.NodeTables.Where(x => x.NodeId == id).FirstOrDefault();
+                NodeTable table = new NodeTable()
+                {
+                    NodeId = data.NodeId,
+                    NodeName = data.NodeName,
+                    ParentNodeId = data.ParentNodeId,
+                    IsActive = data.IsActive,
+                    StartDate = data.StartDate
+                };
+                return RedirectToAction("NodePartialView");
+            }
+        }
+
+        public  List <SelectListItem> NodeDropdownlist()
+        {
+            var itemlist = new List<SelectListItem>();
+            itemlist.Add(new SelectListItem { Value = "0", Text = "---select---" });
+            try
+            {
+                using (DB_AnkitEntities db = new DB_AnkitEntities())
+                {
+                    var item = (from m in db.NodeTables
+                                select new { m.NodeId, m.NodeName }).ToList();
+                    foreach (var data in item )
+                    {
+                        //itemlist.Add(new SelectListItem { Value = data.NodeId.ToString(), Text = data.NodeName.ToString() });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+
+            }
+            return itemlist;
+
+        }
+
+    }
+}
